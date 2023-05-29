0f1c260c53130d7a903458a7c7c28be45dba30e6
Athi <Athi@192.168.1.36>
2023-05-29 13:08:46 +0530

    29-May

---
+++ Scripts/Notepad/Save A Text File/Script1685343814652.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Windows.startApplicationWithTitle('C:\\Windows\\notepad.exe', 'Untitled - Notepad')
+
+Windows.setText(findWindowsObject('Object Repository/Notepad/Text Editor'), 'Hello World')
+
+Windows.click(findWindowsObject('Object Repository/Notepad/MenuItem-File'))
+
+Windows.click(findWindowsObject('Object Repository/Notepad/MenuItem(1)-Save'))
+
+Windows.setText(findWindowsObject('Object Repository/Windows/Edit-FileName'), 'Hello World.txt')
+
+Windows.click(findWindowsObject('Object Repository/Windows/TreeItem-Desktop'))
+
+Windows.click(findWindowsObject('Object Repository/Windows/Button-SaveFile'))
+
+Windows.click(findWindowsObject('Object Repository/Windows/Button(1)-ReplaceExistingFile'))
+
+Windows.closeApplication()
+
