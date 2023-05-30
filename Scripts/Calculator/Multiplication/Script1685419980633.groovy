005f7d58f9af50dd81a59ac181a5a8bc37b70b39
Athi <Athi@192.168.1.36>
2023-05-30 09:51:56 +0530

    30-May

---
+++ Scripts/Calculator/Multiplication/Script1685419980633.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Windows.startApplicationWithTitle('C:\\Users\\Abhi\\Downloads\\Calc 64-bit\\calc1.exe', 'Calculator')
+
+//12*5=60
+Windows.click(findWindowsObject('Object Repository/Calculator/1'))
+
+Windows.click(findWindowsObject('Object Repository/Calculator/2'))
+
+Windows.click(findWindowsObject('Object Repository/Calculator/Multiply'))
+
+Windows.click(findWindowsObject('Object Repository/Calculator/5'))
+
+Windows.click(findWindowsObject('Object Repository/Calculator/Equals'))
+
+String txt = Windows.getText(findWindowsObject('Object Repository/Calculator/ResultArea'))
+
+Windows.verifyMatch(txt.trim(), '60', true)
+
+Windows.closeApplication()
+
